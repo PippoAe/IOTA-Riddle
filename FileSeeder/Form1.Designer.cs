2b3cc22324dd48f2ccf3858c9a27ce70061027cc
PippoAe <philipp.aeberhard@gmx.net>
2018-04-03 19:53:30 +0200

    Projektdateien hinzufügen.

---
+++ FileSeeder/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace FileSeeder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblSeed = new System.Windows.Forms.Label();
+            this.lblHash = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SHA512 base64:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Generated Seed:";
+            // 
+            // lblSeed
+            // 
+            this.lblSeed.AutoSize = true;
+            this.lblSeed.Location = new System.Drawing.Point(140, 129);
+            this.lblSeed.Name = "lblSeed";
+            this.lblSeed.Size = new System.Drawing.Size(100, 17);
+            this.lblSeed.TabIndex = 4;
+            this.lblSeed.Text = "Seed Output...";
+            this.lblSeed.Click += new System.EventHandler(this.lblSeed_Click);
+            // 
+            // lblHash
+            // 
+            this.lblHash.AutoSize = true;
+            this.lblHash.Location = new System.Drawing.Point(140, 99);
+            this.lblHash.Name = "lblHash";
+            this.lblHash.Size = new System.Drawing.Size(80, 17);
+            this.lblHash.TabIndex = 5;
+            this.lblHash.Text = "File-Hash...";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(288, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(350, 55);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Drop File Here";
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(970, 157);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblHash);
+            this.Controls.Add(this.lblSeed);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "IOTA Riddle";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblSeed;
+        private System.Windows.Forms.Label lblHash;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ FileSeeder/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+//Turn a file into an IOTA seed.
+namespace FileSeeder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            //Get FilePath from Drag&Drop
+            string[] FileList = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            byte[] fileBytes = File.ReadAllBytes(FileList[0]);
+
+            //Get SHA512 from file
+            string FileHash = Convert.ToBase64String(GenerateSHA512(fileBytes));
+            lblHash.Text = FileHash;
+
+            //Create Seed from file
+            lblSeed.Text = GenerateSeed(fileBytes);
+        }
+
+        //Generate SHA512 from file
+        public static byte[] GenerateSHA512(byte[] input)
+        {
+            using (SHA512 sha512 = SHA512Managed.Create())
+            {
+                byte[] hash = sha512.ComputeHash(input);
+                return hash;
+            }
+        }
+
+        //This function is a first approach to generate a seed from a file.
+        //This could be done alot better, but I have no clue how to do this yet :)
+        //Suggestions from the community are absolutely welcome.
+        public static string GenerateSeed(byte[] input)
+        {
+            //GetSHA512 Hash of InputFile
+            byte[] inputFileHash = GenerateSHA512(input);
+
+            //Convert Hash to base64 String
+            string b64Hash = Convert.ToBase64String(inputFileHash);
+
+            //Strip Hash from non seed chars
+            string seedChunk = StripStringFromInvalidChars(b64Hash);
+
+            //Rehash original hash until enough valid seed characters are gathered and a seed can be formed from it.
+            while (seedChunk.Length <= 81)
+            {
+                //Rehash previous hash and add it to the chunk 
+                inputFileHash = GenerateSHA512(inputFileHash);
+                b64Hash = Convert.ToBase64String(inputFileHash);
+                seedChunk += StripStringFromInvalidChars(b64Hash);
+            }
+            //Return the seed
+            return seedChunk.Substring(0, 81);
+        }
+
+
+        //Remove all chars from the input that are not allowed in a seed.
+        public static string StripStringFromInvalidChars(string input)
+        {
+            var allowedChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ9";
+            return new string(input.Where(c => allowedChars.Contains(c)).ToArray());
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+
+        //Copy seed to clipboard.
+        private void lblSeed_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(lblSeed.Text);
+        }
+    }
+}
